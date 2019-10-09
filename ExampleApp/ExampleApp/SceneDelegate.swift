27db9353ec1ba4b2818dc4c0aa4abbab7ad6c3d6
Jan Misar <misar.jan@gmail.com>
2019-10-09 09:41:31 +0200

    Start app without storyboard.

--- ExampleApp/ExampleApp/SceneDelegate.swift
+++ ExampleApp/ExampleApp/SceneDelegate.swift
@@ -17,7 +17,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        // Zde můžeme inicializovat vlastní window, s vlastním rootVC a úplně se vyhnout storyboardu
+        // Zakomentováno, jen pro ukázku - pokračujeme v použití storyboardu
+//        let vc = UIViewController()
+//        vc.view.backgroundColor = .green
+//
+//        let window = UIWindow(windowScene: scene)
+//        window.rootViewController = vc
+//        self.window = window
+//        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
