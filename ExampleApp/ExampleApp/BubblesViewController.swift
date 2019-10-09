45c71b1f89326e342d8957e7498cd77a597292aa
Jan Misar <misar.jan@gmail.com>
2019-10-09 09:56:14 +0200

    Rename ViewController to BubblesVC.

--- ExampleApp/ExampleApp/BubblesViewController.swift
+++ ExampleApp/ExampleApp/BubblesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  BubblesViewController.swift
 //  ExampleApp
 //
 //  Created by Jan Misar on 09/10/2019.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class BubblesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
