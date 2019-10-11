4d3fe608908413f1b70f83e3a2bb5f85017dc7b6
Jan Misar <misar.jan@gmail.com>
2019-10-11 21:51:02 +0200

    Task solution - finished BubbleVC.

--- ExampleApp/ExampleApp/BubblesViewController.swift
+++ ExampleApp/ExampleApp/BubblesViewController.swift
@@ -27,6 +27,8 @@ class BubblesViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    var bubbleViews: [UIView] = []
 
     @IBAction func fireButtonTapped(_ sender: UIButton) {
         print("Fire! \(random(min: 0, max: 1))")
@@ -37,12 +39,43 @@ class BubblesViewController: UIViewController {
         // - random barva
         // - všechny kuličky musí být aspoň částí viditelné na obrazovce
         // - tlačítko fire musí být always on top
+
+        //########## ŘEŠENÍ ##########
         
-        // aView.subviews
-        // aView.removeFromSuperview()
+        // odstranění existujících bublin
+        for bubbleView in bubbleViews {
+            bubbleView.removeFromSuperview()
+        }
+        
+        // "vynulování" uložených bublin
+        bubbleViews = []
+        
+        for _ in (0..<20) {
+            let bubbleView = generateRandomBubble()
+            
+            // přidání do self.view
+            view.addSubview(bubbleView)
+            
+            // uložení do pole všech bublin pro pozdější snadnější odstranění
+            bubbleViews.append(bubbleView)
+        }
+        
+        // přesunutí fire buttonu do popředí
+        view.bringSubviewToFront(fireButton)
     }
     
-    func random(min: CGFloat, max: CGFloat) -> CGFloat {
+    private func generateRandomBubble() -> UIView {
+        let randomSize = random(min: 1, max: 80)
+        let bubbleView = UIView(frame: CGRect(x: random(min: 0, max: view.frame.width),
+                                              y: random(min: 0, max: view.frame.height),
+                                              width: randomSize,
+                                              height: randomSize))
+        bubbleView.layer.cornerRadius = randomSize / 2
+        bubbleView.backgroundColor = UIColor(red: random(), green: random(), blue: random(), alpha: 1)
+        return bubbleView
+    }
+    
+    func random(min: CGFloat = 0, max: CGFloat = 1) -> CGFloat {
         let rand = CGFloat(arc4random()) / 0xFFFFFFFF
         return rand * (max - min) + min
     }
