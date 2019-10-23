e0e035dbc3d8b1436f5383eae8c27ca22169eff4
Dominik Vesely <dominik.vesely@ackee.cz>
2019-10-23 12:27:44 +0200

    add pinch

--- ExampleApp/ExampleApp/ShapeView.swift
+++ ExampleApp/ExampleApp/ShapeView.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class ShapeView : UIView {
+class ShapeView : UIView  {
     
     var action : () -> () = {
         print("action")
@@ -23,21 +23,38 @@ class ShapeView : UIView {
         super.init(frame: frame)
         let gestureTap = UITapGestureRecognizer(target: self, action: #selector(ShapeView.tapGesture(gesture:)))
         let doubleTap = UITapGestureRecognizer(target: self, action: #selector(ShapeView.doubleTapGesture(gesture:)))
+        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(scalePiece(_:)))
+
         gestureTap.numberOfTapsRequired = 1
         doubleTap.numberOfTapsRequired = 2
         gestureTap.require(toFail: doubleTap)
-        
+                
         addGestureRecognizer(gestureTap)
         addGestureRecognizer(doubleTap)
+        addGestureRecognizer(pinch)
     }
+    
+    
     
     @objc func tapGesture(gesture : UITapGestureRecognizer) {
         action()
     }
     
     @objc func doubleTapGesture(gesture : UITapGestureRecognizer) {
-           print("action2")
-       }
+        print("action2")
+    }
+    
+    @objc func scalePiece(_ gestureRecognizer : UIPinchGestureRecognizer) {
+        guard gestureRecognizer.view != nil else { return }
+        
+        if gestureRecognizer.state == .began || gestureRecognizer.state == .changed {
+            gestureRecognizer.view?.transform = (gestureRecognizer.view?.transform.scaledBy(x: gestureRecognizer.scale, y: gestureRecognizer.scale))!
+            gestureRecognizer.scale = 1.0
+        }
+    }
+    
+    
+    
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
