17a910d4d6bf96b1fef0552a1c93375a4b28f1e9
Pacek <jan.pacek@strv.com>
2019-11-06 11:08:58 +0100

    lifecycle methods

--- ExampleApp/ExampleApp/ViewConttrollers/AutolayoutedBubblesVC.swift
+++ ExampleApp/ExampleApp/ViewConttrollers/AutolayoutedBubblesVC.swift
@@ -25,7 +25,31 @@ class AutolayoutedBubblesVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        print(segue.destination)
     }
     
     var bubbleViews: [UIView] = []
