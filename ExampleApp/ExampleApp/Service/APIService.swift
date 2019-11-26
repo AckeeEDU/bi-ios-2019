2679b82f0a40751874a8384d8290c7e0b0bc9f2e
Lukáš Hromadník <lukas.hromadnik@ackee.cz>
2019-11-26 15:00:37 +0100

    Show mapping error

--- ExampleApp/ExampleApp/Service/APIService.swift
+++ ExampleApp/ExampleApp/Service/APIService.swift
@@ -8,18 +8,32 @@
 
 import Foundation
 
+struct Recipe: Decodable {
+    let id: String
+    let name: String
+    let duration: Int
+    let score: Int
+}
+
 final class APIService {
     static let shared = APIService()
     private init() { }
 
     func fetchRecipes() {
         let url = URL(string: "https://cookbook.ack.ee/api/v1/recipes")!
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            let json = try! JSONSerialization.jsonObject(with: data!)
-            print("[JSON]", json)
-            print("[DATA]", data)
-            print("[RESPONSE]", response)
-            print("[ERROR]", error)
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                print(error)
+                return
+            }
+
+            guard let data = data else {
+                print("[NO DATA]")
+                return
+            }
+
+            let result = try! JSONDecoder().decode([Recipe].self, from: data)
+            print(result)
         }
         task.resume()
     }
