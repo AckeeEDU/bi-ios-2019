44f00bb80f1b88ad948932a0f9f3e87c02c3fa7d
Lukáš Hromadník <lukas.hromadnik@ackee.cz>
2019-11-27 09:09:07 +0100

    Rewrite some requests with alamofire

--- ExampleApp/ExampleApp/Service/APIService.swift
+++ ExampleApp/ExampleApp/Service/APIService.swift
@@ -7,28 +7,19 @@
 //
 
 import Foundation
+import Alamofire
 
 final class APIService {
     static let shared = APIService()
     private init() { }
 
     func fetchRecipes(_ completion: @escaping (([Recipe]) -> Void)) {
-        let url = URL(string: "https://cookbook.ack.ee/api/v1/recipes")!
-        let task = URLSession.shared.dataTask(with: url) { data, _, error in
-            if let error = error {
-                print(error)
-                return
-            }
-
-            guard let data = data else {
-                print("[NO DATA]")
-                return
-            }
-
+        let url = "https://cookbook.ack.ee/api/v1/recipes"
+        Alamofire.request(url).responseData { response in
+            guard let data = response.data else { print("[NO DATA]"); return }
             let result = try! JSONDecoder().decode([Recipe].self, from: data)
             completion(result)
         }
-        task.resume()
     }
 
     func fetchRecipeDetail(recipe: Recipe, _ completion: @escaping ((RecipeDetail) -> Void)) {
@@ -51,21 +42,33 @@ final class APIService {
     }
 
     func rate(_ score: Int, recipe: Recipe, _ completion: @escaping (() -> Void)) {
-        let url = URL(string: "https://cookbook.ack.ee/api/v1/recipes/\(recipe.identifier)/ratings")!
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        let data = ["score": score]
-        request.httpBody = try! JSONSerialization.data(withJSONObject: data)
+        let url = "https://cookbook.ack.ee/api/v1/recipes/\(recipe.identifier)/ratings"
 
-        let task = URLSession.shared.dataTask(with: request) { data, _, error in
-            if let error = error {
-                print(error)
-                return
+//        var request = URLRequest(url: url)
+//        request.httpMethod = "POST"
+//        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+//        let data = ["score": score]
+//        request.httpBody = try! JSONSerialization.data(withJSONObject: data)
+
+//        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+//            if let error = error {
+//                print(error)
+//                return
+//            }
+//
+//            completion()
+//        }
+//        task.resume()
+
+        Alamofire
+            .request(url, method: .post, parameters: ["score": score], encoding: JSONEncoding.default, headers: nil)
+            .response { response in
+                if let error = response.error {
+                    print(error)
+                    return
+                }
+
+                completion()
             }
-
-            completion()
-        }
-        task.resume()
     }
 }
